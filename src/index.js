79ac945bfe68d8018ecb10461b5cc3c8145124ed
Helga312 <may.traum@gmail.com>
2020-09-30 15:08:01 +0300

    feat: max, min, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    let minValue = 0
+
+    if (Array.isArray(array) && array.length) {
+        for (i = 0; i <= array.length - 1; i++) {
+            if (array[i] <= minValue) {
+                minValue = array[i]
+            }
+        }
+    } else return 0
+    return minValue
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    let maxValue = 0
+
+    if (Array.isArray(array) && array.length) {
+        for (i = 0; i <= array.length - 1; i++) {
+            if (maxValue <= array[i]) {
+                maxValue = array[i]
+            }
+        }
+    } else return 0
+
+    return maxValue
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let avg = 0
+    let arrSum = 0
+
+    if (Array.isArray(array) && array.length) {
+        for (i = 0; i <= array.length - 1; i++) {
+            arrSum += array[i]
+        }
+        avg = arrSum / array.length
+    } else return 0
+
+    return avg
+
 }
